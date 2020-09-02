f5ccff745894acd8b36dc5e448d1820f23d9e8d6
Christie Bahlai <cbahlai@kent.edu>
2020-09-02 15:18:39 -0400

    incorporating Rass's edits

--- plots.R
+++ plots.R
@@ -3,36 +3,43 @@ library(plyr)
 library(ggplot2)
 library(gridExtra)
 library(lubridate)
+library(stringr)
+library(grid)
 
 #create comparative fisheries plot
 
 #will use mcr, ntl and sbc fish data
 
-#going to brute force it for first approximation
+#for sbc, lobster fisheries are most important monitored taxon- see this citation:
+#Zellmer, A. J., Claisse, J. T., Williams, C. M., and Pondella, D. J. (2018). Long-term, spatial marine harvest intensity as an indicator of human impact on shallow rocky reef ecosystems. Mar. Ecol. 39, 1–9. doi:10.1111/maec.12463.
+#Red sea urchin is the biggest fishery but not really targeted along the coast where we have LTER sites.  After that is rock crab (which we don't sample) and spiny lobster (which we do).  Thus, I think spiny lobster would be our best bet.
 
-sbc<-read.csv(file="https://portal.edirepository.org/nis/dataviewer?packageid=knb-lter-sbc.50.7&entityid=24d18d9ebe4f6e8b94e222840096963c", 
+
+
+sbc<-read.csv(file="https://portal.edirepository.org/nis/dataviewer?packageid=knb-lter-sbc.77.4&entityid=f32823fba432f58f66c06b589b7efac6", 
               header=T, na.strings=c("",".","NA", -99999,-99999.00))
 summary(sbc)
 
-#only want the fish
+#sites where fishing is legal
+fished.sites<-c("MOHK", "AQUE", "CARP")
 
-sbc.1<-sbc[which(sbc$COARSE_GROUPING=="FISH"),]
 
-#get a list of common names so we can pull those out later
+#there! ok, let's pull out only the fished, er, fishes
+#we also want to cut off fishes less than 15cm length, which are not targetted by fishers
 
-unique(sbc.1$COMMON_NAME)
+sbc.fished<-sbc[which(sbc$SITE%in%fished.sites),]
 
-#ok, let's just turn this into raw abundance per sampling year- turns out multiple dates are
-#just the same sampling event
 
-sbc$isodate<-ymd(sbc$DATE)
 
-summary.sbc<-ddply(sbc.1, c("YEAR", "SITE", "TRANSECT"), summarise,
-                         tot.biomass=sum(DRY_GM2, na.rm=T))
+
+summary.sbc<-ddply(sbc.fished, c("YEAR", "SITE", "TRANSECT"), summarise,
+                         count=sum(COUNT, na.rm=T), traps=length(COUNT))
+
+summary.sbc$pertrap<-summary.sbc$count/summary.sbc$traps
 
 summary.sbc.bysite<-ddply(summary.sbc, "YEAR", summarise,
-                          mean.mass=mean(tot.biomass), 
-                          se.mass=(sd(tot.biomass)/sqrt(length(tot.biomass))))
+                          mean.count=mean(pertrap), 
+                          se.count=(sd(pertrap)/sqrt(length(pertrap))))
 
 
 plot(summary.sbc.bysite$YEAR, summary.sbc.bysite$mean.mass)
@@ -55,11 +62,29 @@ summary(mcr)
 
 unique(mcr$Taxonomy)
 
+#pull out genera
+mcr$genus<-word(mcr$Taxonomy, 1)
+
+#read in list of fished genera 
+fished<-read.csv(file="MCR_fished_genera.csv", 
+              header=T, stringsAsFactors = F)
+
+unique(mcr$genus)
+
+mcr$fished<-mcr$genus%in%fished$genus
+
+#there! ok, let's pull out only the fished, er, fishes
+#we also want to cut off fishes less than 15cm length, which are not targetted by fishers
+
+mcr.fished<-mcr[which(mcr$fished==TRUE&mcr$Total_Length>15),]
+
+
 #ok, let's just turn this into raw biomass per sampling year- turns out multiple dates are
 #just the same sampling event
 
 
-summary.mcr<-ddply(mcr, c("Year", "Site", "Transect", "Swath"), summarise,
+
+summary.mcr<-ddply(mcr.fished, c("Year", "Site", "Transect", "Swath"), summarise,
                    tot.biomass=(sum(Biomass, na.rm=T))/1000)
 
 summary.mcr.bysite<-ddply(summary.mcr, "Year", summarise,
@@ -141,21 +166,21 @@ ntl.timeseries
 
 #now sbc
 
-sbc.timeseries<-ggplot(summary.sbc.bysite, aes(YEAR, mean.mass))+
+sbc.timeseries<-ggplot(summary.sbc.bysite, aes(YEAR, mean.count))+
   geom_smooth(method="loess", se=T, color="darkblue", fill="azure3", linetype="dotted")+
   geom_line(color="darkcyan", size=1)+
   #geom_errorbar(aes(ymin=(mean.mass-se.mass), ymax=(mean.mass+se.mass)), width=0.2)+
   geom_point(pch=21, color="black", fill="darkcyan", size=2)+
   theme_classic(base_size = 12)+
   xlab(NULL)+
-  ylab(expression(paste("Biomass, g")))+
+  ylab(expression(paste("Lobsters per trap")))+
   xlim(1980,2022)+
   theme(axis.line.x = element_blank(), axis.ticks.x=element_blank(),
         axis.text.x=element_blank())+
-  annotate("text", x=1981, y=30, label="SBC", size=6)+
-  geom_segment(aes(x=2018, xend=2022, y=20, yend=25), 
+  annotate("text", x=1981, y=1.45, label="SBC", size=6)+
+  geom_segment(aes(x=2019, xend=2022, y=1.3, yend=1.6), 
                colour="grey51", linetype="longdash", size=1)+ 
-  geom_segment(aes(x=2018, xend=2022, y=10, yend=15), 
+  geom_segment(aes(x=2019, xend=2022, y=0.9, yend=1.2), 
                colour="grey51", linetype="longdash", size=1)
 
 
@@ -169,25 +194,25 @@ mcr.timeseries<-ggplot(summary.mcr.bysite, aes(Year, mean.mass))+
   geom_point(pch=21, color="black", fill="deepskyblue4", size=2)+
   theme_classic(base_size = 12)+
   xlab(NULL)+
-  ylab(expression(paste("Biomass, g")))+
+  ylab(expression(paste("Fishable biomass (g)")))+
   xlim(1980,2022)+
   theme(axis.line.x = element_blank(), axis.ticks.x=element_blank(),
         axis.text.x=element_blank())+
-  annotate("text", x=1981, y=30, label="MCR", size=6)+
-  geom_segment(aes(x=2018, xend=2022, y=25, yend=20), 
+  annotate("text", x=1981, y=18, label="MCR", size=6)+
+  geom_segment(aes(x=2018, xend=2022, y=13, yend=8), 
                colour="grey51", linetype="longdash", size=1)+ 
-  geom_segment(aes(x=2018, xend=2022, y=10, yend=5), 
+  geom_segment(aes(x=2018, xend=2022, y=8, yend=3), 
                colour="grey51", linetype="longdash", size=1)
 
 mcr.timeseries
 
-grid.draw(rbind(ggplotGrob(mcr.timeseries), ggplotGrob(sbc.timeseries), 
+grid.draw(rbind(ggplotGrob(sbc.timeseries), ggplotGrob(mcr.timeseries),
                 ggplotGrob(ntl.timeseries),
                 size="first"))
 
 
 pdf("fish_timeseries.pdf", height=5, width=8)
-grid.draw(rbind(ggplotGrob(mcr.timeseries), ggplotGrob(sbc.timeseries), 
+grid.draw(rbind(ggplotGrob(sbc.timeseries), ggplotGrob(mcr.timeseries),
                 ggplotGrob(ntl.timeseries),
                 size="first"))
 dev.off()
